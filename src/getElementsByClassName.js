a2e05abd31d1c6bd732aba609dd802bab54303d3
henimezrani <henimezrani@gmail.com>
2019-11-19 14:29:04 +0100

    testing promander

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,28 +7,28 @@
 var getElementsByClassName = function(className, element, arr
 ) {
 
-	var arr = arr || []
+  var arr = arr || [];
 
-	if (arguments[1] === undefined) {
-		element = document.body;
-		if(element.classList.contains(className)) {
-			arr.push(element);
-		}
-	}
+  if (arguments[1] === undefined) {
+    element = document.body;
+    if (element.classList.contains(className)) {
+      arr.push(element);
+    }
+  }
 
-	if (element.childNodes.length === 0) {
-		return arr;
-	}
+  if (element.childNodes.length === 0) {
+    return arr;
+  }
 
-	if (element.nodeType === 1) {
-		for (var i = 0; i < element.childNodes.length; i++) {
-			if (element.childNodes[i].nodeType === 1) {
-				if(element.childNodes[i].classList.contains(className)) {
-					arr.push(element.childNodes[i]);
-				}
-				getElementsByClassName(className, element.childNodes[i], arr)
-			}
-		}
-	}
-	return arr;
+  if (element.nodeType === 1) {
+    for (var i = 0; i < element.childNodes.length; i++) {
+      if (element.childNodes[i].nodeType === 1) {
+        if (element.childNodes[i].classList.contains(className)) {
+          arr.push(element.childNodes[i]);
+        }
+        getElementsByClassName(className, element.childNodes[i], arr);
+      }
+    }
+  }
+  return arr;
 };
